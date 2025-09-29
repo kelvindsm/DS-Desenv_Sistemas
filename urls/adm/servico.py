e19190bd8d634361788029868ff8d4c02508126a
kelvin.martins <kelvin.martins@sempreceub.com>
2025-09-29 20:58:52 -0300

    Definição do Insert - Incluir servico e setor

---
+++ urls/adm/servico.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template, request
+
+from database.setor_dao import SetorDAO
+from database.servico_dao import ServicoDAO
+
+bp_serv = Blueprint('serv', __name__, url_prefix='/adm/serv')
+
+
+@bp_serv.route('/incluir')  # /adm/serv/incluir
+def incluir():
+    dao = SetorDAO()
+    lst_setores = dao.read_by_filters([('sts_setor', '=', 'A')])
+    return render_template('adm/serv/incluir.html', msg="", css_msg="", lst_setores=lst_setores)
+
+
+@bp_serv.route('/salvar_incluir', methods=['POST'])  # /adm/serv/salvar_incluir
+def salvar_incluir():
+    dao = ServicoDAO()
+    serv = dao.new_object()
+    serv.nme_servico = request.form['nme_servico']
+    serv.num_dias_servico = request.form['num_dias_servico']
+    serv.vlr_servico = request.form['vlr_servico']
+    serv.txt_modelo_servico = request.form['txt_modelo_servico']
+    serv.sts_servico = request.form['sts_servico']
+    serv.cod_setor = request.form['cod_setor']
+
+    if dao.insert(serv):
+        msg = f"Serviço número {serv.idt_servico} inserido com sucesso!"
+        css_msg = "sucesso"
+    else:
+        msg = "Erro ao tentar incluir serviço!"
+        css_msg = "erro"
+
+    dao_setor = SetorDAO()
+    lst_setores = dao_setor.read_by_filters([('sts_setor', '=', 'A')])
+
+    return render_template('adm/serv/incluir.html', msg=msg, css_msg=css_msg, lst_setores=lst_setores)
