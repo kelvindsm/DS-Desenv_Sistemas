dc4a6b796c4dd39dad9cda1d1cf2f94710543096
kelvin.martins <kelvin.martins@sempreceub.com>
2025-10-06 20:53:10 -0300

    Definição do Select - Consultar servico e setor

--- urls/adm/setor.py
+++ urls/adm/setor.py
@@ -9,6 +9,20 @@ def incluir():
     return render_template('adm/setor/incluir.html', msg="", css_msg="")
 
 
+@bp_setor.route('/consultar')  # /adm/setor/consultar
+def consultar():
+    return render_template('adm/setor/consultar.html', setores=[], filtro_usado='')
+
+
+@bp_setor.route('/roda_consultar', methods=['POST'])  # /adm/setor/roda_consultar
+def roda_consultar():
+    nme_setor = request.form['nme_setor']
+    filtro_usado = f'Nome do Setor: {nme_setor}'
+    dao = SetorDAO()
+    setores = dao.read_by_like('nme_setor', nme_setor)
+    return render_template('adm/setor/consultar.html', setores=setores, filtro_usado=filtro_usado)
+
+
 @bp_setor.route('/salvar_incluir', methods=['POST'])  # /adm/setor/salvar_incluir
 def salvar_incluir():
     dao = SetorDAO()
